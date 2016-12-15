3e75732616776cf9cf0a927717a8fa99f8df03e3
maxime <maxime.marquet1@gmail.com>
2016-12-15 12:18:02 +0100

    Ajout du css, du js, de la police d'icone. Initialisation du header, footer, index, et functions .php. Test de la librairie css/js dans header et footer .php

---
+++ wp-content/themes/verygoodtrip/footer.php
@@ -0,0 +1,11 @@
+        <?php
+
+
+        ?>
+        <script type="text/javascript">
+            $( document ).ready(function() {
+                $('.ui.dropdown').dropdown();
+            });
+        </script>
+    </body>
+</html>
