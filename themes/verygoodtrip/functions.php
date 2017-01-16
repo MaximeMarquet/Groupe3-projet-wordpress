4737bf94d3834225d97cef3ff50ea0bae5a08e75
ayuladio <armelyuladio@gmail.com>
2017-01-16 21:27:03 +0100

    base du projet

---
+++ themes/verygoodtrip/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+/* Enqueue Styles and Scripts */
+function my_scripts_styles() {
+	wp_enqueue_style('main-style', get_template_directory_uri().'/css/customstyle.css');
+    wp_enqueue_style('semantic-css', get_template_directory_uri() . '/css/semantic.min.css');
+    wp_enqueue_script('jquery-js', get_template_directory_uri() . '/js/jquery.min.js');
+    wp_enqueue_script('semantic-js', get_template_directory_uri() . '/js/semantic.min.js');
+}
+
+add_action('wp_enqueue_scripts', 'my_scripts_styles');
+
+
+/* Image à la UNE */
+add_theme_support('post-thumbnails');
+
+add_action('init', 'add_image_sizes');
+function add_image_sizes() {
+    add_image_size( 'thumbnail-medium', 400, 300, TRUE ); // force crop true
+}// end
+
+
+
+/* Modifie la taille des extraits (nb mots)  */
+function new_excerpt_length($length) {
+return 30;
+}
+add_filter('excerpt_length', 'new_excerpt_length');
---
+++ themes/verygoodtrip/index.php
@@ -0,0 +1,51 @@
+<?php
+    get_header();
+?>
+ 
+
+    <div class="container">
+		<section class="three-col three-last-article">
+
+			<?php
+			    $recentPosts = new WP_Query();
+			    $recentPosts->query('showposts=3');
+			?>
+			<?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+				<article class="col grey">
+					<a href="<?php the_permalink() ?>" class="article-thumbnail" rel="bookmark">
+							<?php
+								the_post_thumbnail('thumbnail-medium'); // Afficher l'image à la UNE
+							?>
+					</a>
+					<div class="article-content">
+						<h2 class="article-title">		
+							<?php 
+								the_title(); 
+							?>
+						</h2>
+						<p class="article-extract">
+							<?php
+								the_excerpt();
+								// Afficher l'extrait
+							?>
+						</p>
+						<div class="read-more">
+							<a href="<?php the_permalink() ?>" class="ui right labeled icon button" >
+							  <i class="right arrow icon"></i>
+							  Lire la suite
+							</a>
+						</div>
+					</div>
+				</article>
+			<?php endwhile; ?>		
+
+		</section>	
+
+    </div>
+
+
+
+<?php
+
+
+    get_footer();
