8fa85b3e335daffa07c1b06b821a5a1c9ebcf139
maxime <maxime.marquet1@gmail.com>
2017-02-03 12:45:47 +0100

    header + footer + css/js

--- themes/verygoodtrip/functions.php
+++ themes/verygoodtrip/functions.php
@@ -2,10 +2,35 @@
 
 /* Enqueue Styles and Scripts */
 function my_scripts_styles() {
-	wp_enqueue_style('main-style', get_template_directory_uri().'/css/customstyle.css');
     wp_enqueue_style('semantic-css', get_template_directory_uri() . '/css/semantic.min.css');
+
+
+
+    wp_enqueue_style('reset-css', get_template_directory_uri().'/css/components/reset.css');
+    wp_enqueue_style('site-css', get_template_directory_uri().'/css/components/site.css');
+    wp_enqueue_style('container-css', get_template_directory_uri().'/css/components/container.css');
+    wp_enqueue_style('grid-css', get_template_directory_uri().'/css/components/grid.css');
+    wp_enqueue_style('image-css', get_template_directory_uri().'/css/components/image.css');
+    wp_enqueue_style('menu-css', get_template_directory_uri().'/css/components/menu.css');
+    wp_enqueue_style('divider-css', get_template_directory_uri().'/css/components/divider.css');
+    wp_enqueue_style('dropdown-css', get_template_directory_uri().'/css/components/dropdown.css');
+    wp_enqueue_style('segment-css', get_template_directory_uri().'/css/components/segment.css');
+    wp_enqueue_style('button-css', get_template_directory_uri().'/css/components/button.css');
+    wp_enqueue_style('list-css', get_template_directory_uri().'/css/components/list.css');
+    wp_enqueue_style('icon-css', get_template_directory_uri().'/css/components/icon.css');
+    wp_enqueue_style('sidebar-css', get_template_directory_uri().'/css/components/sidebar.css');
+    wp_enqueue_style('transition-css', get_template_directory_uri().'/css/components/transition.css');
+
+    wp_enqueue_style('main-style', get_template_directory_uri().'/css/customstyle.css');
+
+
+
     wp_enqueue_script('jquery-js', get_template_directory_uri() . '/js/jquery.min.js');
     wp_enqueue_script('semantic-js', get_template_directory_uri() . '/js/semantic.min.js');
+    wp_enqueue_script('visibility-js', get_template_directory_uri() . '/css/components/visibility.min.js');
+    wp_enqueue_script('sidebar-js', get_template_directory_uri() . '/css/components/sidebar.min.js');
+    wp_enqueue_script('transition-js', get_template_directory_uri() . '/css/components/transition.min.js');
+
 }
 
 add_action('wp_enqueue_scripts', 'my_scripts_styles');
--- themes/verygoodtrip/header.php
+++ themes/verygoodtrip/header.php
@@ -12,87 +12,37 @@
         ?>
     </head>
     <body>
-        <?php
-            echo 'test';
-        ?>
-        <div class="ui labeled button" tabindex="0">
-            <div class="ui red button">
-                <i class="heart icon"></i> Like
-            </div>
-            <a class="ui basic red left pointing label">
-                1,048
-            </a>
-        </div>
-        <div class="ui labeled button" tabindex="0">
-            <div class="ui basic blue button">
-                <i class="fork icon"></i> Forks
-            </div>
-            <a class="ui basic left pointing blue label">
-                1,048
-            </a>
-        </div>
+        <div class="ui fixed inverted menu">
+            <div class="ui container" style="padding: 0 !important;">
+                <a href="<?php echo get_home_url(); ?>" class="header item">
+                    <?php bloginfo('name'); ?>
+                </a>
 
-        <select class="ui search dropdown">
-          <option value="">State</option>
-          <option value="AL">Alabama</option>
-          <option value="AK">Alaska</option>
-          <option value="AZ">Arizona</option>
-          <option value="AR">Arkansas</option>
-          <option value="CA">California</option>
-          <option value="CO">Colorado</option>
-          <option value="CT">Connecticut</option>
-          <option value="DE">Delaware</option>
-          <option value="DC">District Of Columbia</option>
-          <option value="FL">Florida</option>
-          <option value="GA">Georgia</option>
-          <option value="HI">Hawaii</option>
-          <option value="ID">Idaho</option>
-          <option value="IL">Illinois</option>
-          <option value="IN">Indiana</option>
-          <option value="IA">Iowa</option>
-          <option value="KS">Kansas</option>
-          <option value="KY">Kentucky</option>
-          <option value="LA">Louisiana</option>
-          <option value="ME">Maine</option>
-          <option value="MD">Maryland</option>
-          <option value="MA">Massachusetts</option>
-          <option value="MI">Michigan</option>
-          <option value="MN">Minnesota</option>
-          <option value="MS">Mississippi</option>
-          <option value="MO">Missouri</option>
-          <option value="MT">Montana</option>
-          <option value="NE">Nebraska</option>
-          <option value="NV">Nevada</option>
-          <option value="NH">New Hampshire</option>
-          <option value="NJ">New Jersey</option>
-          <option value="NM">New Mexico</option>
-          <option value="NY">New York</option>
-          <option value="NC">North Carolina</option>
-          <option value="ND">North Dakota</option>
-          <option value="OH">Ohio</option>
-          <option value="OK">Oklahoma</option>
-          <option value="OR">Oregon</option>
-          <option value="PA">Pennsylvania</option>
-          <option value="RI">Rhode Island</option>
-          <option value="SC">South Carolina</option>
-          <option value="SD">South Dakota</option>
-          <option value="TN">Tennessee</option>
-          <option value="TX">Texas</option>
-          <option value="UT">Utah</option>
-          <option value="VT">Vermont</option>
-          <option value="VA">Virginia</option>
-          <option value="WA">Washington</option>
-          <option value="WV">West Virginia</option>
-          <option value="WI">Wisconsin</option>
-          <option value="WY">Wyoming</option>
-        </select>
+                <?php 
+                    $categories = get_categories(array(
+                            'parent' => 0
+                        ));
 
-        <div>
-          <ul>
-          <?php 
-            wp_list_categories();
-           ?>
-          </ul>
+                    foreach($categories as $category) {
+                        $categoriesChilds = get_categories(array(
+                            'parent' => $category->term_id
+                        ));
+
+                        if(count($categoriesChilds) != 0) {
+                            echo '<div class="ui simple dropdown item">';
+                                echo $category->name . '<i class="dropdown icon"></i>';
+                                echo '<div class="menu">';
+                                    foreach($categoriesChilds as $cat){
+                                        echo '<a href="' . get_category_link($cat) . '" class="item">' . $cat->name . '</a>';
+                                    }
+                                echo '</div>';
+                            echo '</div>';
+                        } else{
+                            echo '<a href="' . get_category_link($category) . '" class="item">' . $category->name . '</a>';
+                        }
+                    }
+                ?>
+            </div>
         </div>
 
         <div class="slider">
