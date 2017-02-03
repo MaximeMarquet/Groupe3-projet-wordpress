aac4e85cb13fb80486b1fc4f60067052788dbcb5
Laurent Bassin <laurent@bassin.info>
2017-02-03 10:26:33 +0100

    Correction

---
+++ themes/verygoodtrip/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+get_header();
+
+
+if(have_posts()){
+    while (have_posts()){
+        the_post();
+        echo '<h2>'.get_the_title().'</h2>';
+        the_post_thumbnail('medium'); // Afficher l'image à la UNE
+        the_content();
+    }
+} else{
+    echo 'Aucun article';
+}
+
+
+$loop = new WP_Query(array('post_type'=>'events'));
+while ( $loop->have_posts()){
+	$loop->the_post();
+	the_title();
+	the_content();
+}
+
+
+get_footer();
+
+?>
+
+
