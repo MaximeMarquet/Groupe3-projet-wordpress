f6290b8473100aa165f6372897c190bc434564b9
maxime <maxime.marquet1@gmail.com>
2017-02-23 19:35:17 +0100

    footer

--- themes/verygoodtrip/footer.php
+++ themes/verygoodtrip/footer.php
@@ -2,26 +2,27 @@
             <div class="ui container">
             <div class="ui stackable inverted divided equal height stackable grid">
                 <div class="three wide column">
-                <h4 class="ui inverted header">About</h4>
+                <h4 class="ui inverted header">Lorem</h4>
                 <div class="ui inverted link list">
-                    <a href="#" class="item">Sitemap</a>
-                    <a href="#" class="item">Contact Us</a>
-                    <a href="#" class="item">Religious Ceremonies</a>
-                    <a href="#" class="item">Gazebo Plans</a>
+                    <a href="#" class="item">Lorem</a>
+                    <a href="#" class="item">Lorem</a>
+                    <a href="#" class="item">Lorem</a>
+                    <a href="#" class="item">Lorem</a>
                 </div>
                 </div>
                 <div class="three wide column">
-                <h4 class="ui inverted header">Services</h4>
+                <h4 class="ui inverted header">Lorem</h4>
                 <div class="ui inverted link list">
-                    <a href="#" class="item">Banana Pre-Order</a>
-                    <a href="#" class="item">DNA FAQ</a>
-                    <a href="#" class="item">How To Access</a>
-                    <a href="#" class="item">Favorite X-Men</a>
+                    <a href="#" class="item">Lorem</a>
+                    <a href="#" class="item">Lorem</a>
+                    <a href="#" class="item">HLorem</a>
+                    <a href="#" class="item">Lorem</a>
                 </div>
                 </div>
                 <div class="seven wide column">
-                <h4 class="ui inverted header">Footer Header</h4>
-                <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                <h4 class="ui inverted header">Lorem</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet magna eget iaculis sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi nisi, aliquet non viverra eget, hendrerit eleifend enim. Praesent finibus tortor at scelerisque varius. Etiam malesuada eros lobortis neque ullamcorper, quis aliquet arcu ornare.
+                </p>
                 </div>
             </div>
             </div>
